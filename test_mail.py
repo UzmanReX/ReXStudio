187da9d3e4a94f268b7b0351e103ecd5ac6e1b30
UzmanReX <galatasaray01efe@gmail.com>
2026-09-19 14:20:49 +0300

    Update test_mail.py

--- test_mail.py
+++ test_mail.py
@@ -1,26 +1,29 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 
-GMAIL_ADRESI = "rexstudiosupport@gmail.com"
-UYGULAMA_SIFRESI = "plxytbetznrlwlzz"
+GMAIL_ADRESI = os.environ["rexstudiosupport@gmail.com"]
+GMAIL_UYGULAMA_SIFRESI = os.environ["plxytbetznrlwlzz"]
+TEST_ALICI = os.environ["galatasaray01efe@gmail.com"]
 
-ALICI = "galatasaray01efe@gmail.com"
-
-mesaj = MIMEText("""
-Merhaba!
+mesaj = MIMEText(
+    """Merhaba!
 
 Bu ReXStudio test mailidir.
 
 Gmail kod gönderme sistemi çalışıyor.
-""", "plain", "utf-8")
+""",
+    "plain",
+    "utf-8"
+)
 
 mesaj["Subject"] = "ReXStudio Test"
 mesaj["From"] = GMAIL_ADRESI
-mesaj["To"] = ALICI
+mesaj["To"] = TEST_ALICI
 
 with smtplib.SMTP("smtp.gmail.com", 587) as server:
     server.starttls()
-    server.login(GMAIL_ADRESI, UYGULAMA_SIFRESI)
+    server.login(GMAIL_ADRESI, GMAIL_UYGULAMA_SIFRESI)
     server.send_message(mesaj)
 
 print("Mail başarıyla gönderildi!")
